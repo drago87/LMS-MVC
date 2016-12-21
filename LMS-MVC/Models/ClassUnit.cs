038fda559de52f61fcf26ff97dc487a6e8f9d048
drago87 <dr.drago.87@hotmail.com>
2016-12-21 09:51:07 +0100

    Added old model files

---
+++ LMS-MVC/Models/ClassUnit.cs
@@ -0,0 +1,25 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace LMS.Models
+{
+    public class ClassUnit
+    {
+        [Key]
+        public int ClassUnitID { get; set; }
+
+        public string ClassName { get; set; }
+
+        public ICollection<ApplicationUser> Participants { get; set; }
+
+        public Folder Shared { get; set; }
+
+        public Folder Submission { get; set; }
+
+    }
+}
