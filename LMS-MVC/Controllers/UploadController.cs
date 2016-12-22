df6f4e96638ee229f72e9cbedcc3487daa945b84
Marcus <marcus_bok@msn.com>
2016-12-22 09:42:54 +0100

    FileUpload

---
+++ LMS-MVC/Controllers/UploadController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS_MVC.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ActionResult UploadDocument()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    //var path = Path.Combine(Server.MapPath(@"C:\Files"), fileName);
+
+                    file.SaveAs(@"C:\Files\" + fileName);
+                }
+            }
+
+            return RedirectToAction("UploadDocument");
+        }
+    }
+}
