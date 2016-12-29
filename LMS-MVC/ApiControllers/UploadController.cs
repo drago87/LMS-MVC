aef97921543d70fccebb0971fda205ff73e2d8d7
Marcus <marcus_bok@msn.com>
2016-12-29 13:11:53 +0100

    uploadChange

--- LMS-MVC/ApiControllers/UploadController.cs
+++ LMS-MVC/ApiControllers/UploadController.cs
@@ -12,39 +12,39 @@ using System.Web.Http;
 
 namespace LMS_MVC.Controllers
 {
-    public class UploadController : ApiController
+    public class UploadMVCController : ApiController
     {
-        SharedRepository _repo = new SharedRepository();
+        //SharedRepository _repo = new SharedRepository();
 
 
-        [Authorize(Roles = "Teacher, Student")]
-        public IHttpActionResult UploadDocument()
-        {
-            return Ok();
-        }
+        //[Authorize(Roles = "Teacher, Student")]
+        //public IHttpActionResult UploadDocument()
+        //{
+        //    return Ok();
+        //}
 
-        [HttpPost]
-        public IHttpActionResult Upload()
-        {
-            if (Request.Files.Count > 0)
-            {
-                var file = Request.Files[0];
+        //[HttpPost]
+        //public IHttpActionResult Upload()
+        //{
+        //    if (Request.Files.Count > 0)
+        //    {
+        //        var file = Request.Files[0];
 
-                if (file != null && file.ContentLength > 0)
-                {
-                    var fileName = Path.GetFileName(file.FileName);
-                    //var path = Path.Combine(Server.MapPath(@"C:\Files"), fileName);
-                    var path = @"C:\Files\" + fileName;
-                    file.SaveAs(path);
-                    var user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+        //        if (file != null && file.ContentLength > 0)
+        //        {
+        //            var fileName = Path.GetFileName(file.FileName);
+        //            //var path = Path.Combine(Server.MapPath(@"C:\Files"), fileName);
+        //            var path = @"C:\Files\" + fileName;
+        //            file.SaveAs(path);
+        //            var user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
 
-                    _repo.AddFileToClassUnitShared(user, new Dossier { FileName = file.FileName, FilePath = path });
+        //            _repo.AddFileToClassUnitShared(user, new Dossier { FileName = file.FileName, FilePath = path });
                     
-                }
-            }
+        //        }
+        //    }
 
-            return RedirectToAction("UploadDocument");
-        }
+        //    return RedirectToAction("UploadDocument");
+        //}
     }
 }
 
