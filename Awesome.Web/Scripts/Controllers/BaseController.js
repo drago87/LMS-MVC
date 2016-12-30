07c651b188ccfdce58614fd15bbee821bda19cc2
Anders Lundström <lundstrom123@gmail.com>
2016-12-30 16:00:38 +0100

    städat bort den angular app som var här

--- Awesome.Web/Scripts/Controllers/BaseController.js
+++
@@ -1,8 +0,0 @@
-var BaseController = function($scope, SessionService){
-	$scope.helloAgain = 'Hello Angular 1.3!';
-	
-	$scope.loggedIn = function(){
-		return SessionService.getToken() !== undefined;
-	}
-}
-BaseController.$inject = ['$scope', 'SessionService'];
