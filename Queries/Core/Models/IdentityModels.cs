397e4fb4094f085b07f96bc3a8cf40d7b28f9dba
Anders Lundström <lundstrom123@gmail.com>
2017-01-09 09:21:43 +0100

    usercontroller, index-vy

--- Queries/Core/Models/IdentityModels.cs
+++ Queries/Core/Models/IdentityModels.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Queries.Core.Domain;
 using Queries.Data.SampleData;
 
-//namespace bat_mvc.Models
 namespace Queries.Core.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
