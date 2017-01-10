677486f7645d69e91d818c185f878800c1df3804
drago87 <dr.drago.87@hotmail.com>
2017-01-10 13:24:29 +0100

    FileViewModel recreated

---
+++ bat-mvc/ViewModels/FileViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace bat_mvc.ViewModels
+{
+    public class FileViewModel
+    {
+        public FileInfo File { get; set; }
+
+        public string IconImg { get; set; }
+    }
+}
