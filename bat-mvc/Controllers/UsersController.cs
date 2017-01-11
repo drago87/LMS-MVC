ed415a0e659dc80cc39b977ee3f35469c25384b2
drago87 <dr.drago.87@hotmail.com>
2017-01-11 13:55:36 +0100

    onödig kod

--- bat-mvc/Controllers/UsersController.cs
+++ bat-mvc/Controllers/UsersController.cs
@@ -160,43 +160,6 @@ namespace bat_mvc.Controllers
             return View(model);
         }
 
-        [Authorize(Roles = "Teacher")]
-        //[AllowAnonymous]
-        public ActionResult Register2()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/Register
-        [HttpPost]
-        //[AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register2(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    //await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Users");
-                }
-                AddErrors(result);
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
 
         #region Helpers
         private void AddErrors(IdentityResult result)
